c2474ca7f85e312f389ac357160c3427d3177394
Kevin <klums24@gmail.com>
2023-04-05 18:20:26 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+//define multiply as equation of two numbers that equal 62
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+
+//define random to be a random integer greater than 0
+const random = Math.ceil(Math.random());
+
+
+//define mod to be remainder that equals 4
+const num3 = 30;
+const num4 = 13;
+
+const mod = num3 % num4;
+
+//define max to be highest number in a set (20)
+
+const max = Math.max(12, 7, 19, 5, 20, 3);
+
